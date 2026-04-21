c1453177438e1ca122f43f72dc841736641b78dd
rxsh <rushwi16@outlook.com>
2026-04-20 20:52:14 -0500

    multithread linked list

---
+++ Multithread LL/linkedListMultithread.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+struct Nodo{
+
+    int data;
+    Nodo* next;
+};
+
+class LinkedList{
+
+    Nodo* head;
+    Nodo* cola;
+
+    void insertar(){
+
+    }
+
+    void search(){
+
+    }
+
+    void delet(){
+
+    }
+
+    void print(){
+        
+    }
+
+
+};
